4a71e586890a5e4b240ec5060918bb678d00367c
Daniel Callus <danielcallus@gmail.com>
2021-04-08 14:21:28 +0100

    files setup

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.test_rps import RpsTest
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_rps.py
@@ -0,0 +1,6 @@
+import unittest
+from src.rps import Rps
+
+class RockPaperScissorTest(unittest.TestCase):
+
+    def 
